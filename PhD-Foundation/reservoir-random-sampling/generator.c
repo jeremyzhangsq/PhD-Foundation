b37da8600171044212161722acf2f733660876f6
jeremyzhangsq <jeremyzhangsq@gmail.com>
2019-07-26 17:17:45 +0800

    generate random integer records

---
+++ PhD-Foundation/reservoir-random-sampling/generator.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]){
+	int n = atoi(argv[1]);
+	FILE *fp;
+	fp = fopen("record.txt","w");
+	for(int i = 0; i<n; i++){
+		fprintf(fp,"%d\t", rand()%10000);
+	}
+	fprintf(fp,"\n");
+	fclose(fp);
+	return 0;
+}
