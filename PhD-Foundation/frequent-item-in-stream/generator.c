7c23741a5ce3684b573ea42620704cc7023a2399
jeremyzhangsq <jeremyzhangsq@gmail.com>
2019-07-31 18:11:51 +0800

    frequent counting algorithm (in double linked list)

--- PhD-Foundation/frequent-item-in-stream/generator.c
+++ PhD-Foundation/frequent-item-in-stream/generator.c
@@ -8,10 +8,7 @@ int main(int argc, char* argv[]){
 	int r;
 	for(int i = 0; i<n; i++){
 		r = rand();
-		if((double)r/RAND_MAX<0.7)
-			fprintf(fp,"%d\t", 2);
-		else
-			fprintf(fp,"%d\t", r%10);
+		fprintf(fp,"%d\t", r%10);
 	}
 	fprintf(fp,"\n");
 	fclose(fp);
--- PhD-Foundation/frequent-item-in-stream/main.cpp
+++ PhD-Foundation/frequent-item-in-stream/main.cpp
@@ -3,8 +3,32 @@
 #include <stdio.h>
 #include <vector>
 #include <unordered_map>
+#include <list>
+#include <iterator>
+#include <unordered_set>
+
 using namespace std;
 
+class Group{
+public:
+    int delta;
+    int capacity;
+    unordered_set<int> data;
+    Group(int i,int j){
+        delta = i;
+        capacity = j;
+    }
+    Group(int i){
+        delta = i;
+        capacity = 0;
+    }
+    void add(int &i){
+        data.insert(i);
+    }
+    int get(int &i){
+        return *(data.find(i));
+    }
+};
 void Majority(vector<int> &arr){
     int count=0;
     int majority=0;
@@ -43,11 +67,76 @@ void BruteFore(vector<int> &arr, int k){
     }
     printf("Brute Force Result:");
     for(auto ele:dict){
-        if(ele.second>=k)
+        if(ele.second>k)
             printf("%d\t",ele.first);
     }
     printf("\n");
 }
+
+list<Group>::iterator find(list<Group> &groups,int i){
+
+    list<Group>::iterator g;
+    for(g = groups.begin();g!=groups.end();g++){
+        if (g->data.count(i))
+            return g;
+    }
+    return groups.end();
+
+}
+
+void Frequent(vector<int> &arr, int k){
+    int m = arr.size()/k-1;
+    list<Group> groups;
+    Group g0(0,m);
+    Group g1(1);
+    // insert head and tail into list
+    groups.push_back(g0);
+    groups.push_back(g1);
+    for(int &ele:arr){
+        auto g = find(groups,ele);
+        // no count represents this element and some counts exist
+        if(g==groups.end() and groups.front().delta == 0 and groups.front().capacity){
+            auto head = groups.begin();
+            head->capacity--;
+            auto nxt = next(head,1);
+            nxt->add(ele);
+        }
+        // the element is represented by a count
+        else if(g!=groups.end()){
+            g->data.erase(ele);
+            auto nxt = next(g,1);
+            // move it to next group
+            if(nxt == groups.end()){
+                Group gc(1);
+                gc.add(ele);
+                groups.push_back(gc);
+
+            }else
+                nxt->data.insert(ele);
+
+        }
+        // decrement
+        else{
+            auto first = groups.begin();
+            for(;first!=groups.end();first++){
+                if(first->delta>=1){
+                    first->delta--;
+                    first->capacity=first->data.size();
+                    groups.erase(prev(first,1));
+                    return;
+                }
+            }
+        }
+    }
+    printf("Frequent Counting Result:");
+    for(auto it = next(groups.begin(),k);it!=groups.end();it++){
+        for(int ele: it->data){
+            printf("%d\t",ele);
+        }
+    }
+    printf("\n");
+}
+
 
 int main() {
     FILE *fp;
@@ -59,7 +148,8 @@ int main() {
         data.push_back(val);
     }
     Majority(data);
-    BruteFore(data,30);
+    BruteFore(data,8);
+    Frequent(data,8);
     return 0;
 }
 
